87be8a0026761552cdab9dba25b77126b6982ade
Kat Maldon <katharinamaldon@gmail.com>
2019-05-03 14:56:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,41 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create 
+    @@all << self.new
+  end 
+  
+  def self.new_by_name(song_name) 
+    
+  end 
+  
+  def create_by_name 
+    
+  end 
+  
+  def self.find_by_name(name) 
+    @@all.find{|song| song.name == name }
+  end 
+  
+  def find_or_create_by_name 
+    
+  end 
+  
+  def self.alphabetical 
+    return self.all.sort
+  end 
+  
+  def new_from_filename 
+    
+  end 
+  
+  def create_from_filename 
+    
+  end
+  
+  def self.destroy_all 
+    self.all.clear
+  end 
 
 end
